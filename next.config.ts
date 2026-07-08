a5a572ef48432625b89d32fbca1682a382fa25ce
itshemanthcode <intern01.dhoond@gmail.com>
2026-07-08 14:43:06 +0530

    final updates

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   basePath: '/ews',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ews',
+        basePath: false,
+        permanent: false,
+      },
+      {
+        source: '/checkout',
+        destination: '/ews/checkout',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
