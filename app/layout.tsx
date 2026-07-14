46619c9f2477cfa7cfa6804cc542f4b650d568a1
itshemanthcode <intern01.dhoond@gmail.com>
2026-07-14 12:48:28 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1350936867182583');
+          fbq('init', '3960157500788009');
           fbq('track', 'PageView');
         ` }} />
         <noscript>
@@ -85,7 +85,7 @@ export default function RootLayout({
             height="1" 
             width="1" 
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1350936867182583&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=3960157500788009&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
